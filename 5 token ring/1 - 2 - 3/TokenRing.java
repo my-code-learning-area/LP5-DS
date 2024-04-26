2ad7703bcd7661c2e208f153943dd425401bc324
Sumit Kawale - ubu <sumitkawale2002@gmail.com>
2024-04-27 03:15:56 +0530

    saving

--- 5 token ring/1 - 2 - 3/TokenRing.java
+++ 5 token ring/1 - 2 - 3/TokenRing.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class TokenRing {
     public static void main(String args[]) {
@@ -6,12 +6,13 @@ public class TokenRing {
 
         System.out.print("Enter the number of nodes: \t");
         int n = sc.nextInt();
-        
+
         // Decide the number of nodes forming the ring
         int token = 0;
 
-        for(int i = 0; i < n; i++) 
+        for(int i = 0; i < n; i++) {
             System.out.print(" " + i);
+        }
         System.out.println(" " + 0);
 
         try {
@@ -24,18 +25,20 @@ public class TokenRing {
                 String data = sc.next();
 
                 System.out.print("\n\nToken Passing:");
+
                 // current token not equal to sender, increment i and j by (j + 1) % n
-                for(int i = token, j = token; s != (i % n); i = j = (j + 1) % n) {
-                    System.out.print(" " + j + "->");
+                for(int i = token; (i % n) != s; i = (i + 1) % n) {
+                    System.out.print(" " + i + "->");
                 }
                 System.out.println(" " + s);
-                System.out.println("Sender " + s + " sending data: " + data);
 
                 // start forwarding from node after sender until it becomes equal to receiver and increment  by (i + 1) % n
+                System.out.println("Sender " + s + " sending data: " + data);
                 for(int i = (s + 1) % n; i != r; i = (i + 1) % n) {
                     System.out.println("Data " + data + " forwarded by " + i);
                 }
                 System.out.println("Receiver " + r + " received data: " + data);
+
                 token = s;
             }
         } catch (Exception e) {
